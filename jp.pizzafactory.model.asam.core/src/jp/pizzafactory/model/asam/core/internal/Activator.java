ea49f7bfbbf8a8dce7404776fe901b865eacf1fc
Masaki Muranaka <monaka@monami-ya.com>
2016-01-26 14:23:30 +0900

    Add getContent() to supress a warning.

--- jp.pizzafactory.model.asam.core/src/jp/pizzafactory/model/asam/core/internal/Activator.java
+++ jp.pizzafactory.model.asam.core/src/jp/pizzafactory/model/asam/core/internal/Activator.java
@@ -10,13 +10,16 @@ public class Activator extends Plugin {
     private static BundleContext context;
 
     @Override
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-	}
+    public void start(BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
+    }
 
     @Override
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
+    public void stop(BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
 
+    public static BundleContext getContext() {
+        return context;
+    }
 }
